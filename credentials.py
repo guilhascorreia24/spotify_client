16e06b55de9ffc38623f53417b179971670d1c5a
Guilhas24 <55436745+guilhascorreia24@users.noreply.github.com>
2023-10-29 19:20:24 +0000

    Delete credentials.py

--- credentials.py
+++
@@ -1,7 +0,0 @@
-client_ID='858457b5ac6a47fda5c65e2effc1c950'       # Fill in with the value from the Spotify Developer Dashboard
-client_SECRET='51cb7df89ad24a13813b358250ae9a9a'   # Fill in with the value from the Spotify Developer Dashboard
-redirect_URI="http://127.0.0.1:8000/auth/callback"  # Fill in with the port you want to use -- MUST be one of the 
-                                                                   # redirect urls as in Spotify Developer Dashboard 
-username=''        # Fill in with Spotify username you want to use                                                                                                      
-scope='ugc-image-upload user-read-private user-read-email'
-
